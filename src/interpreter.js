5e5dc2ac6993886304d49288f7cddbeeadcfd4ca
honeycomb <tarakosuki98525mk2@gmail.com>
2021-02-06 00:09:45 +0900

    インタープリター作成

--- src/interpreter.js
+++ src/interpreter.js
@@ -31,15 +31,112 @@ var Pointer = /** @class */ (function () {
     };
     return Pointer;
 }());
-// init
-var memory_size = 100;
-var memory = [];
-for (var i = 0; i < memory_size; i++) {
-    memory.push(new Cell());
+var Instruction = /** @class */ (function () {
+    function Instruction(inc_v, dec_v, inc_p, dec_p, output, input, jump_f, jump_b) {
+        if (inc_v === void 0) { inc_v = '+'; }
+        if (dec_v === void 0) { dec_v = '-'; }
+        if (inc_p === void 0) { inc_p = '>'; }
+        if (dec_p === void 0) { dec_p = '<'; }
+        if (output === void 0) { output = '.'; }
+        if (input === void 0) { input = ','; }
+        if (jump_f === void 0) { jump_f = '['; }
+        if (jump_b === void 0) { jump_b = ']'; }
+        this.inc_v = inc_v;
+        this.dec_v = dec_v;
+        this.inc_p = inc_p;
+        this.dec_p = dec_p;
+        this.output = output;
+        this.input = input;
+        this.jump_f = jump_f;
+        this.jump_b = jump_b;
+    }
+    return Instruction;
+}());
+function memory_init(mem_size) {
+    if (mem_size === void 0) { mem_size = 1000; }
+    var memory = [];
+    for (var i = 0; i < mem_size; i++) {
+        memory.push(new Cell());
+    }
+    return memory;
+}
+function interpreter(instructions, input, memory, ptr) {
+    var out = "";
+    if (input.length > 0) {
+        var crt_input = input[0];
+    }
+    else {
+        var crt_input = '';
+    }
+    for (var i = 0; i < instructions.length; i++) {
+        switch (instructions[i]) {
+            case '+':
+                memory[ptr.index].increment();
+                break;
+            case '-':
+                memory[ptr.index].decrement();
+                break;
+            case '>':
+                ptr.right();
+                break;
+            case '<':
+                ptr.left();
+                break;
+            case '.':
+                out += memory[ptr.index].putChar();
+                break;
+            case ',':
+                memory[ptr.index].setChar(crt_input);
+                input = input.slice(1);
+                break;
+            case '[':
+                if (memory[ptr.index].value == 0) {
+                    var open_bracket_num = 0;
+                    var idx = i + 1;
+                    while (1) {
+                        if (instructions[idx] == '[') {
+                            open_bracket_num++;
+                        }
+                        else if (instructions[idx] == ']' && open_bracket_num == 0) {
+                            i = idx + 1;
+                            break;
+                        }
+                        else if (instructions[idx] == ']' && open_bracket_num > 0) {
+                            open_bracket_num--;
+                        }
+                        idx++;
+                    }
+                }
+                break;
+            case ']':
+                if (memory[ptr.index].value != 0) {
+                    var close_bracket_num = 0;
+                    var idx = i - 1;
+                    while (1) {
+                        if (instructions[idx] == ']') {
+                            close_bracket_num++;
+                        }
+                        else if (instructions[idx] == '[' && close_bracket_num == 0) {
+                            i = idx;
+                            break;
+                        }
+                        else if (instructions[idx] == '[' && close_bracket_num > 0) {
+                            close_bracket_num--;
+                        }
+                        idx--;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    return out;
 }
 var ptr = new Pointer();
+var input = "";
 // exec instructions
 // 対応するカッコはスタックで探す
-function exec(instructions) {
-    console.log(instructions);
+function exec(instructions, input, memory, ptr) {
+    return interpreter(instructions, input, memory, ptr);
 }
--- src/interpreter.ts
+++ src/interpreter.ts
@@ -1,3 +1,4 @@
+
 class Cell {
     value: number;
 
@@ -42,17 +43,113 @@ class Pointer {
     }
 }
 
+class Instruction {
+    inc_v: string;
+    dec_v: string;
+    inc_p: string;
+    dec_p: string;
+    output: string;
+    input: string;
+    jump_f: string;
+    jump_b: string;
 
-// init
-let memory_size: number = 100;
-let memory = [];
-for (let i = 0; i < memory_size; i++) {
-    memory.push(new Cell());
+    constructor(inc_v:string = '+', dec_v: string = '-', inc_p: string = '>', dec_p: string = '<', output: string = '.', input: string = ',', jump_f: string = '[', jump_b: string = ']'){
+        this.inc_v = inc_v;
+        this.dec_v = dec_v;
+        this.inc_p = inc_p; 
+        this.dec_p = dec_p; 
+        this.output = output; 
+        this.input = input; 
+        this.jump_f = jump_f;  
+        this.jump_b = jump_b;
+    }
 }
-let ptr = new Pointer();
+
+function memory_init(mem_size: number = 1000){
+    let memory = [];
+    for (let i = 0; i < mem_size; i++) {
+        memory.push(new Cell());
+    }
+    return memory;
+}
+
+function interpreter(instructions: string, input: string, memory: Cell[], ptr: Pointer):string {
+    var out = "";
+    if (input.length > 0) {
+        var crt_input = input[0];
+    } else {
+        var crt_input = '';
+    }
+
+    for(let i = 0; i < instructions.length; i++) {
+        switch (instructions[i]) {
+            case '+':
+                memory[ptr.index].increment();
+                break;
+            case '-':
+                memory[ptr.index].decrement();
+                break;
+            case '>':
+                ptr.right();
+                break;
+            case '<':
+                ptr.left();
+                break;
+            case '.':
+                out += memory[ptr.index].putChar();
+                break;
+            case ',':
+                memory[ptr.index].setChar(crt_input);
+                input = input.slice(1);
+                break;
+            case '[':
+                if (memory[ptr.index].value == 0) {
+                    let open_bracket_num: number = 0;
+                    let idx: number = i + 1;
+                    while (1) {
+                        if ( instructions[idx] == '[' ) {
+                            open_bracket_num++;
+                        } else if ( instructions[idx] == ']' && open_bracket_num == 0 ) {
+                            i = idx+1;
+                            break;
+                        } else if ( instructions[idx] == ']' && open_bracket_num > 0 ) {
+                            open_bracket_num--;
+                        }
+                        idx++;
+                    } 
+                }
+                break;
+            case ']':
+                if (memory[ptr.index].value != 0) {
+                    let close_bracket_num: number = 0;
+                    let idx: number = i - 1;
+                    while (1) {
+                        if ( instructions[idx] == ']' ) {
+                            close_bracket_num++;
+                        } else if ( instructions[idx] == '[' && close_bracket_num == 0) {
+                            i = idx;
+                            break;
+                        } else if ( instructions[idx] == '[' && close_bracket_num > 0 ){
+                            close_bracket_num--;
+                        }
+                        idx--;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    return out;
+}
+
+
+var ptr = new Pointer();
+var input = "";
+
 
 // exec instructions
 // 対応するカッコはスタックで探す
-function exec(instructions: string):void {
-    console.log(instructions);
+function exec(instructions: string, input: string, memory: Cell[], ptr: Pointer):string {
+    return interpreter(instructions, input, memory, ptr);
 }
